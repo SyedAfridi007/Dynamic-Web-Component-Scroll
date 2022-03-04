fb5957d25d8df9b23f9e16c3ac16fe4414a8ab3c
Md. Syed Afridi <83403859+SyedAfridi007@users.noreply.github.com>
2022-03-04 21:10:30 +0600

    Add files via upload

---
+++ Round Scroll Percentge/script.js
@@ -0,0 +1,16 @@
+ 
+let scrollPercentage = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round( pos * 100 / calcHeight);
+    scrollProgress.style.background = `conic-gradient(#e2d30c ${scrollValue}%, #2b2f38 ${scrollValue}%)`;
+    progressValue.textcontent = `${scrollValue}%`;
+
+}
+window.onscroll = scrollPercentage;
+window.onload = scrollPercentage;
+
+
+    
