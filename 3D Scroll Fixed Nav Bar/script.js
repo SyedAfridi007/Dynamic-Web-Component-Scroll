51cce08485429aa2851b06161a64680440aed9e1
Md. Syed Afridi <83403859+SyedAfridi007@users.noreply.github.com>
2022-03-04 21:21:49 +0600

    Add files via upload

---
+++ 3D Scroll Fixed Nav Bar/script.js
@@ -0,0 +1,27 @@
+$(window).scroll(function(){
+    var heroHeight = $('header').height();
+    var yPosition = $(document).scrollTop();
+
+    if(yPosition <= heroHeight){
+        var effectFactor = yPosition / heroHeight;
+        var rotation = effectFactor * (Math.PI / 2- Math.asin(
+            (heroHeight -yPosition) /heroHeight));
+    }
+
+    $('.hero').css({
+        '-webkit-transform': 'rotateX('+rotation+'rad)',
+        'transform': 'rotateX('+rotation+'rad)',
+    })
+
+    .find('.overlay').css('opacity',effectFactor);
+
+
+
+if(yPosition <= heroHeight){
+    $('nav').removeClass('fixed');
+}
+else{
+    $('nav').addClass('fixed');
+}
+
+})
